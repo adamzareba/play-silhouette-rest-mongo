fb8be2eff801cd55ebd1f383f1565390291c6c12
Adam Zaręba <adam.zareba@softwarehut.pl>
2017-10-05 17:22:05 +0200

    refactored, UI fixes

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import javax.inject.Inject
+import play.api.mvc._
+import javax.inject.Singleton
+
+@Singleton
+class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  /**
+    * Create an Action to render an HTML page with a welcome message.
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+
+}
