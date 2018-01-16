5edac31ec89a068abb854bde7b64d2e1fa6ae01c
Adam Zaręba <adam.zareba@softwarehut.pl>
2018-01-16 23:03:43 +0100

    removed wrong mapping

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -30,12 +30,7 @@ class ApplicationController @Inject()(components: ControllerComponents,
   def redirectDocs = Action { implicit request =>
     Redirect(
       url = "/assets/lib/swagger-ui/index.html",
-      queryString = Map("url" -> Seq(
-        if(request.host.contains(":9000"))
-          "http://" + request.host + "/swagger.json"
-        else
-          "https://" + request.host + "/swagger.json"
-      ))
+      queryString = Map("url" -> Seq(queryString = Map("url" -> Seq("http://" + request.host + "/swagger.json"))))
     )
   }
 
